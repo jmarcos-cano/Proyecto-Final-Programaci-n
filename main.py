20ae30704bca342fba5e7cacaac2c4268bb7ad2d
alejandrosalazar1 <alejandrosalazarb@ufm.edu>
2020-05-14 22:15:17 +0000

    funciones iniciales y menu

--- main.py
+++ main.py
@@ -27,6 +27,42 @@ def list_contacts():
   print("Listar contactos")
   print(contacts)
 
+#Llamar contacto
+def call_contact():
+  print("Llamar contacto")
+
+#Enviar mensaje a contacto
+def send_message():
+  print("Enviar mensaje")
+
+#Enviar email a contacto
+def send_email():
+  print("Enviar email")
+
+#exit
+def exit():
+  print("Salir del programa")
+
+#Menu
+def menu():
+  print("------ Agenda Telefonica --------")
+  print("1. Ingresar contacto")
+  print("2. Buscar contacto")
+  print("3. Lista contactos")
+  print("4. Borrar contacto")
+  print("5. Llamar contacto")
+  print("6. Enviar mensaje a contacto")
+  print("7. Enviar email a contacto")
+  print("8. Exportar contactos")
+  print("9. Salir")
+
+  try: 
+    op = int(input("Escoja una opcion: "))
+    return op
+  except:
+    print("Opcion incorrecta. Porfavor ingrese otra vez.")
+    return None
+  
 
 
 
