f0c80765596da4375c59fad8d1e1aa6c2b06aa18
alejandrosalazar1 <alejandrosalazarb@ufm.edu>
2020-05-14 23:03:55 +0000

    agregue add_contact function y otros arreglos

--- main.py
+++ main.py
@@ -2,30 +2,91 @@
 
 import json
 from urllib.request import urlopen
+import validators 
+import time
 
+#get contacts from JSON and initialize
 def get_contacts(): 
-  url = "http://demo7130536.mockable.io/contacts"
+  """Get the json from the url and return it as a dictionary""" 
+
+  #Change url valuee with the final url
+  url = "http://demo7130536.mockable.io/contacts" 
   response = urlopen(url)
   data = json.loads(response.read())
   return(data)
 
+phone_book = get_contacts()
+
 #Añadir contacto
 def add_contact():
-  print("Añadir contacto")
+  """Request the user to input all contact needed values and add it tio the contacts dictionary"""
+
+  nombre = input("Ingrese el nombre del contacto: ")
+  telefono = input("Ingrese el telefono del contacto:")
+  correo = input("Ingrese el correo del contacto")
+  empresa = input("(Opcional) Ingrese la empresa del contacto")
+  extra = input("(Opcional) Ingrese infirmacion adicional del contacto")
+
+  isOK, stage = validate_contact(nombre, telefono, correo)
+  while isOK == False:
+    if stage == "nombre":
+      print("\n Nombre invalido. El nombre del contacto debe tener 2 palabras")
+      nombre  = input("Ingrese el nombre del contacto: ")
+    elif stage == "telefono":
+      print("\n Telefono invalido. El numero de telefono debe incluir solo numeros")
+      telefono= input("Ingrese el telefono del contacto:")
+    elif stage == "correo":
+      print("\n Correo invalido. El correo debe tener la forma -> ejemplo@example.org")
+      correo = input("Ingrese el correo del contacto:")
+    isOK, stage = validate_contact(nombre, telefono, correo)
+  nombre =  nombre.strip()
+  correo = correo.strip()
+  contact = {
+    "telefono": telefono,
+    "correo": correo,
+    "empresa": empresa,
+    "extra": extra
+  }
+
+  try:
+    old = phone_book[nombre[0].upper()]
+
+    old[nombre] = contact
+  except KeyError:
+    phone_book[nombre[0].upper()] = {nombre: contact}
+  print("\n Usuario agregado con exito!")
+  time.sleep(2)
+
+def validate_contact(nombre, telefono, correo):
+  if len(nombre.split()) !=2:
+    return False, "nombre"
+  elif not str(telefono).isnumeric() or len(telefono) < 1:
+    return False, "telefono"
+  elif not validators.email(correo):
+    return False, "correo"
+  else:
+    return True, "pass"
+
+add_contact()
 
 #Buscar contacto
 def search_contact():
+  """Search and show all contact names which contains the entered keyword"""
+
+  print("Buscar contacto")
+  print(contacts)
+
+#Listar contacto
+def list_contacts():
+  """Show a list of all contacts sorted alphabetically by first letter of their name"""
+
   print("Listar contactos")
   print(contacts)
 
 # Borrar contacto
 def delete_contact():
+  """Request the user for the name ot the number and delete the contact if exists"""
   print("Borrar contacto")
-
-#Listar contacto
-def list_contacts():
-  print("Listar contactos")
-  print(contacts)
 
 #Llamar contacto
 def call_contact():
@@ -44,8 +105,9 @@ def export_contacts():
   print("Exportar")
 
 #exit
-def exit():
-  print("Salir del programa")
+def exit_program():
+  print("Va a salir de su agenda telefonica...")
+  exit()
 
 #Menu
 def menu():
@@ -64,28 +126,36 @@ def menu():
     op = int(input("Escoja una opcion: "))
     return op
   except:
-    print("Opcion incorrecta. Porfavor ingrese otra vez.")
+    print("Opcion incorrecta. Porfavor intete otra vez.")
     return None
   
-contacts = get_contacts()
-
 switcher = {
-  1: add_contact,
-  2: search_contact,
-  3: list_contacts,
-  4: delete_contact,
-  5: call_contact,
-  6: send_message,
-  7: send_email,
-  8: export_contacts,
-  9: exit,
+    1: add_contact,
+    2: search_contact,
+    3: list_contacts,
+    4: delete_contact,
+    5: call_contact,
+    6: send_message,
+    7: send_email,
+    8: export_contacts,
+    9: exit,
 }
 
+"""
 accion = 0 
 while accion != 9:
   accion = menu()
-  func = switcher.get(accion, lambda: print("Opcion incorrecta, intente de nuevo."))
+  func = switcher.get(accion, lambda: print("Opcion incorrecta. Porfavor intente de nuevo."))
   func()
+
+"""
+
+for letter, contacts in phone_book.items():
+  print("letter:",letter)
+  print("contacts:", contacts)
+  for name, info in contacts.items():
+    print("name:", name)
+    print("info", info)
 
 
 
